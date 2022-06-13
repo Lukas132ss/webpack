67046effc5702a95730a1e818207c27f4b296693
Lukas132ss <lk@serrante.com.br>
2022-06-13 11:26:51 -0300

    Aula de Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,45 @@
+const modoDev = process.env.NODE_ENV !== 'production';
 const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
     },
+    devServer: {
+        static: './public',
+        port: 9000
+    },
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                parallel: true,
+                terserOptions: {
+                    ecma: 6,
+                },
+            }),
+            new OptimizeCSSAssetsPlugin({}),
+            
+        ]},
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'estilo.css'
+        }),
+        
+    ],
     module: {
         rules: [{
-            test: /\.css$/,
+            test: /\.s?[ac]ss$/,
             use: [
-                'style-loader', // Adiciona CSS a DOM injetando a tag <style>
-                'css-loader' // Interpreta @import, url()...
+                MiniCssExtractPlugin.loader,
+                // 'style-loader', // Adiciona CSS a DOM injetando a tag <style>
+                'css-loader', // Interpreta @import, url()...
+                'sass-loader' // Compila Sass para CSS
             ]
         }]
     }
